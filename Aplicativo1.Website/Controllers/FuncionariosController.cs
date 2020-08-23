1f58ee6a1ef6b9480f4f76ad034d847662d8a9f2
ruffatto <arthur.ruffatto@gmail.com>
2020-08-23 18:29:14 -0300

    Add project files.

---
+++ Aplicativo1.Website/Controllers/FuncionariosController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Aplicativo1.Website.Controllers
+{
+    public class FuncionariosController : Controller
+    {
+        // GET: Funcionarios
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Listar()
+        {
+            return View();
+        }
+
+        public ActionResult Gravar()
+        {
+            return View();
+        }
+
+        public ActionResult Excluir()
+        {
+            return View();
+        }
+
+        public ActionResult Procurar()
+        {
+            return View();
+        }
+    }
+}
---
+++ Aplicativo1.Website/Controllers/LivrosController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Aplicativo1.Website.Controllers
+{
+    public class LivrosController : Controller
+    {
+        // GET: Livros
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Listar()
+        {
+            return View();
+        }
+
+        public ActionResult Gravar()
+        {
+            return View();
+        }
+
+        public ActionResult Excluir()
+        {
+            return View();
+        }
+
+        public ActionResult Procurar()
+        {
+            return View();
+        }
+
+    }
+}
